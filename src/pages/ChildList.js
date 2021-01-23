f6698d29405e8a0476e862f4c976051c596ed42d
Nico Plyley <nplyley@gmail.com>
2021-01-23 13:44:15 -0500

    Created child list page

---
+++ src/pages/ChildList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Container, Title} from '../components'
+
+const ChildList = () => {
+    return (
+        <Container>
+            <Title>Register Child</Title>
+
+
+        </Container>
+    )
+}
+
+export default ChildList
