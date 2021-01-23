4d363ddfab633227f1b4c424792e933c45ce2d87
Nico Plyley <nplyley@gmail.com>
2021-01-22 20:23:18 -0500

    Created project theme

---
+++ src/Theme.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {ThemeProvider} from 'styled-components'
+
+const theme = {
+    colors: {
+        main: '#3EB6CE'
+    },
+    fonts: {
+        main: 'sans-serif'
+    },
+    media: {
+        phone: '599px',
+        smallTablet: '600px',
+        largeTablet: '900px',
+        laptop: '1200px',
+        desktop: '1800px'
+    },
+    container: {
+        phone: '90%',
+        smallTablet: '570px',
+        largeTablet: '870px',
+        laptop: '1170px',
+        desktop: '1770px'
+    }
+}
+
+export default function Theme({children}) {
+    return <ThemeProvider theme={theme}>{children}</ThemeProvider>
+}
