1c9bddc4fcc2688816c8dd1cde860a47d153060a
Nico Plyley <nplyley@gmail.com>
2021-01-23 12:29:14 -0500

    Created guardian page

---
+++ src/pages/Guardian.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Container, Title} from '../components'
+
+const Guardian = () => {
+    return (
+        <Container>
+            <Title>Guardian Form</Title>
+        </Container>
+    )
+}
+
+export default Guardian
