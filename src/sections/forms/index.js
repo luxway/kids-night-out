a92fd5a2d762f8f3500765fb819999c622623509
Nico Plyley <nplyley@gmail.com>
2021-01-23 15:05:04 -0500

    Exported add child form

--- src/sections/forms/index.js
+++ src/sections/forms/index.js
@@ -1,2 +1,3 @@
 export {default as GuardianForm} from './GuardianForm'
 export {default as EmergencyContactForm} from './EmergencyContactForm'
+export {default as AddChildForm} from './AddChildForm'
