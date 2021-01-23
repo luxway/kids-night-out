2e26e7a89975bedd9662ae0b62fe525489854b9c
Nico Plyley <nplyley@gmail.com>
2021-01-23 13:42:13 -0500

    Created form component

---
+++ src/components/form/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Wrapper, Input, TextArea, InputWrapper, RadioButton, Label, Group, Select, Option} from './styles/form'
+
+const Form = ({children, ...restProps}) => <Wrapper {...restProps}>{children}</Wrapper>
+
+Form.Input = ({...restProps}) => <Input {...restProps} />
+
+Form.TextArea = ({children, ...restProps}) => <TextArea {...restProps}>{children}</TextArea>
+
+Form.RadioButton = ({...restProps}) => <RadioButton {...restProps} />
+
+Form.Wrapper = ({children, ...restProps}) => <InputWrapper {...restProps}>{children}</InputWrapper>
+
+Form.Group = ({children, ...restProps}) => <Group {...restProps}>{children}</Group>
+
+Form.Label = ({children, ...restProps}) => <Label {...restProps}>{children}</Label>
+
+Form.Select = ({children, ...restProps}) => <Select {...restProps}>{children}</Select>
+
+Form.Option = ({children, ...restProps}) => <Option {...restProps}>{children}</Option>
+
+export default Form
---
+++ src/components/form/styles/form.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.form``
+
+export const Input = styled.input`
+    font-size: 18px;
+    padding: 10px;
+`
+
+export const TextArea = styled.textarea``
+
+export const RadioButton = styled.input``
+
+export const InputWrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`
+
+export const Label = styled.label`
+    text-align: left;
+  
+    span {
+      color: red;
+    }
+`
+
+export const Group = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 10px 5px;
+    flex: 1;
+`
+
+export const Select = styled.select`
+  font-size: 18px;
+  padding: 10px;
+`
+
+export const Option = styled.option``
