7a83e3cc56d5d6da1bec82b2e61f46f66994d5c7
Nico Plyley <nplyley@gmail.com>
2021-01-23 15:04:11 -0500

    Added button based off context

--- src/pages/ChildList.js
+++ src/pages/ChildList.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import {Container, Title} from '../components'
+import React, {useContext} from 'react'
+import {Button, Container, Text, Title} from '../components'
+import {ChildContext} from '../App'
+import {Link} from 'react-router-dom'
 
 const ChildList = () => {
+    const [child] = useContext(ChildContext)
+
     return (
         <Container>
-            <Title>Register Child</Title>
-
-
+            <Title>Registered Kids</Title>
+            {child.length < 0 ? 'yes' : (<Text margin="25px 0">No Children Have Been Registered Yet</Text>)}
+            <Button><Link to="/add-child">Add Child</Link></Button>
+            {child.length < 0 ? (<Button margin="25px 0"><Link to="/payment">Continue</Link></Button>) : null}
         </Container>
     )
 }
