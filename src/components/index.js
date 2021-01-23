06c2062ee23624fd6538536ea935678db3188dc9
Nico Plyley <nplyley@gmail.com>
2021-01-22 21:13:02 -0500

    Exported title and text

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export {default as Navigation} from './navigation'
 export {default as Container} from './container'
 export {default as Button} from './button'
+export {default as Title} from './title'
+export {default as Text} from './text'
