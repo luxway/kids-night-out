5b1cfaa70c8301f3c275532b5829a679f9ab2cfd
Nico Plyley <nplyley@gmail.com>
2021-01-23 13:45:23 -0500

    Exported emergency contact and child list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,4 @@
 export {default as Main} from './Main'
 export {default as Guardian} from './Guardian'
+export {default as EmergencyContact} from './EmergencyContact'
+export {default as ChildList} from './ChildList'
