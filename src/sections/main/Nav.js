d0e0163de59797ea062be8f7fa92db90fb970622
Nico Plyley <nplyley@gmail.com>
2021-01-22 20:23:44 -0500

    Created navigation structure

---
+++ src/sections/main/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Navigation} from '../../components'
+
+const Nav = () => {
+    return (
+        <Navigation>
+            <p>Kids Night Out - Registration</p>
+        </Navigation>
+    )
+}
+
+export default Nav
