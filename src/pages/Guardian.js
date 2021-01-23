99f03a4ef04ab892323a20e42ed7ea2f0f88fe30
Nico Plyley <nplyley@gmail.com>
2021-01-23 14:07:34 -0500

    Updated text

--- src/pages/Guardian.js
+++ src/pages/Guardian.js
@@ -6,7 +6,7 @@ import {Link} from 'react-router-dom'
 const Guardian = () => {
     return (
         <Container>
-            <Title margin="0 0 5px 0">Guardian Form</Title>
+            <Title margin="0 0 5px 0">Guardian Info</Title>
             <Text margin="0 0 25px 0" color="red">* Indicates required fields</Text>
             <GuardianForm />
             <Button margin="25px 0"><Link to="/emergency-contact">Continue</Link></Button>
