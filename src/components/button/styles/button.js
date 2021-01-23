9a78707fb45477752c999ca98ff39474d0076921
Nico Plyley <nplyley@gmail.com>
2021-01-22 21:13:22 -0500

    Added styles

--- src/components/button/styles/button.js
+++ src/components/button/styles/button.js
@@ -1,3 +1,22 @@
 import styled from 'styled-components'
 
-export const Inner = styled.button``
+export const Inner = styled.button`
+    background: ${props => props.theme.colors.main};
+    color: #FFF;
+    border: 0;
+    font-size: 18px;
+    border-radius: 50px;
+    opacity: 0.8;
+    transition: 0.2s;
+  
+    a {
+      color: inherit;
+      text-decoration: none;
+      display: block;
+      padding: 25px;
+    }
+  
+    &:hover {
+      opacity: 1;
+    }
+`
