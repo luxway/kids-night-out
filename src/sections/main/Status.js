9b4c43f39a9c7bad9013e9e34545ba1dd016b801
Nico Plyley <nplyley@gmail.com>
2021-01-23 12:29:02 -0500

    Created status layout for breadcrumbs

---
+++ src/sections/main/Status.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import {Breadcrumbs, Container} from '../../components'
+
+const Status = () => {
+    return (
+        <Container>
+            <Breadcrumbs>
+
+                <Breadcrumbs.Item className="line">
+                    <Breadcrumbs.Group>
+                        <Breadcrumbs.Icon />
+                        <Breadcrumbs.Info>
+                            Guardian Info
+                        </Breadcrumbs.Info>
+                    </Breadcrumbs.Group>
+                    <Breadcrumbs.Line />
+                </Breadcrumbs.Item>
+
+                <Breadcrumbs.Item className="line">
+                    <Breadcrumbs.Group>
+                        <Breadcrumbs.Icon />
+                        <Breadcrumbs.Info>
+                            Child Info
+                        </Breadcrumbs.Info>
+                    </Breadcrumbs.Group>
+                    <Breadcrumbs.Line />
+                </Breadcrumbs.Item>
+
+                <Breadcrumbs.Item className="line">
+                    <Breadcrumbs.Group>
+                        <Breadcrumbs.Icon />
+                        <Breadcrumbs.Info>
+                            Payment
+                        </Breadcrumbs.Info>
+                    </Breadcrumbs.Group>
+                    <Breadcrumbs.Line />
+                </Breadcrumbs.Item>
+
+                    <Breadcrumbs.Item>
+                        <Breadcrumbs.Group>
+                            <Breadcrumbs.Icon />
+                            <Breadcrumbs.Info>
+                                Success
+                            </Breadcrumbs.Info>
+                        </Breadcrumbs.Group>
+                    </Breadcrumbs.Item>
+
+
+            </Breadcrumbs>
+        </Container>
+    )
+}
+
+export default Status
