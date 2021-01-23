67f277b71ec5154e896352a3a0373ad6c4f8c795
Nico Plyley <nplyley@gmail.com>
2021-01-23 14:01:30 -0500

    Chnaged home page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Nav, Status} from './sections/main'
 import Theme from './Theme'
 import {GlobalStyle} from './GlobalStyles'
@@ -6,6 +6,10 @@ import {ChildList, EmergencyContact, Guardian, Main} from './pages'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 const App = () => {
+    const [guardian, setGuardian] = useState({})
+    const [emergency, setEmergency] = useState({})
+    const [child, setChild] = useState([])
+
     return (
         <>
             <Theme>
@@ -14,8 +18,7 @@ const App = () => {
                 <Status />
                 <Router>
                     <Switch>
-                        <Route path="/" exact component={Main} />
-                        <Route path="/guardian" component={Guardian} exact />
+                        <Route path="/" component={Guardian} exact />
                         <Route path="/emergency-contact" component={EmergencyContact} exact />
                         <Route path="/child" component={ChildList} exact />
                     </Switch>
