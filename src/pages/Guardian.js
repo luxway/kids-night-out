0c37a08cd7ff9003138ddc4545ede8310b087b33
Nico Plyley <nplyley@gmail.com>
2021-01-23 13:44:41 -0500

    Added form

--- src/pages/Guardian.js
+++ src/pages/Guardian.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import {Container, Title} from '../components'
+import {Container, Title, Text, Button} from '../components'
+import {GuardianForm} from '../sections/forms'
+import {Link} from 'react-router-dom'
 
 const Guardian = () => {
     return (
         <Container>
-            <Title>Guardian Form</Title>
+            <Title margin="0 0 5px 0">Guardian Form</Title>
+            <Text margin="0 0 25px 0" color="red">* Indicates required fields</Text>
+            <GuardianForm />
+            <Button margin="25px 0"><Link to="/emergency-contact">Continue</Link></Button>
         </Container>
     )
 }
