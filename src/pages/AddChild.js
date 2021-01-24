27777ac3a431bf9b2b5e6f167e0e72b316c2d82f
Nico Plyley <nplyley@gmail.com>
2021-01-24 15:49:38 -0500

    Moved button to form

--- src/pages/AddChild.js
+++ src/pages/AddChild.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import {Container, Title, Text, Button} from '../components'
+import {Container, Title, Text} from '../components'
 import {AddChildForm} from '../sections/forms'
-import {Link} from 'react-router-dom'
 
 const AddChild = () => {
     return (
@@ -9,7 +8,6 @@ const AddChild = () => {
             <Title>Register A Child</Title>
             <Text margin="0 0 25px 0" color="red">* Indicated required fields</Text>
             <AddChildForm />
-            <Button margin="25px 0"><Link to='/child'>Add Child</Link></Button>
         </Container>
     )
 }
