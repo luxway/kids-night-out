c480ad71f61bb712393f498c7c52aa2250ee89c5
Nico Plyley <nplyley@gmail.com>
2021-01-24 13:59:04 -0500

    Updated button to use function

--- src/pages/ChildList.js
+++ src/pages/ChildList.js
@@ -1,19 +1,28 @@
 import React, {useContext} from 'react'
 import {Button, Container, Text, Title} from '../components'
 import {ChildContext} from '../App'
-import {Link} from 'react-router-dom'
+import {useHistory} from 'react-router-dom'
 import {ChildrenList} from '../sections/main'
 
 const ChildList = () => {
     const [child] = useContext(ChildContext)
     const childRegistered = child.length < 1
+    const history = useHistory()
+
+    const handleAddClick = () => {
+        history.push('/add-child')
+    }
+
+    const handleContinueClick = () => {
+        history.push('/payment')
+    }
 
     return (
         <Container>
             <Title>Registered Kids</Title>
             {childRegistered ? (<ChildrenList />) : (<Text margin="25px 0">No Children Have Been Registered Yet</Text>)}
-            <Button color="#49DBBB"><Link to="/add-child">Add Child</Link></Button>
-            {childRegistered ? (<Button margin="25px 0"><Link to="/payment">Continue</Link></Button>) : null}
+            <Button onClick={handleAddClick} color="#49DBBB" valid>Add Child</Button>
+            {childRegistered ? (<Button onClick={handleContinueClick} valid margin="25px 0">Continue</Button>) : null}
         </Container>
     )
 }
