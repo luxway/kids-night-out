11b8d0d2b75d83c1bd60519280d0b312a30fd128
Nico Plyley <nplyley@gmail.com>
2021-01-23 20:37:26 -0500

    Added validation logic

--- src/components/button/styles/button.js
+++ src/components/button/styles/button.js
@@ -1,15 +1,16 @@
 import styled from 'styled-components'
 
 export const Inner = styled.button`
-    background: ${props => props.color || props.theme.colors.main};
+    background: ${props => !props.valid ? '#ccc' : props.color || props.theme.colors.main};
     color: #FFF;
     border: 0;
     font-size: 18px;
     border-radius: 50px;
-    opacity: 0.8;
+    opacity: ${props => !props.valid ? '1' : '0.8'};
     transition: 0.2s;
     margin: ${props => props.margin || 0};
     width: ${props => props.width || '100%'};
+    padding: 25px;
   
     a {
       color: inherit;
