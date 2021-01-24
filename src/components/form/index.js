3f835cf3bd243e254aa3a92fb7247c31fd6d40b1
Nico Plyley <nplyley@gmail.com>
2021-01-24 13:52:28 -0500

    Added errors, updated default type

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import {Wrapper, Input, TextArea, InputWrapper, RadioButton, Label, Group, Select, Option} from './styles/form'
+import {Wrapper, Input, TextArea, InputWrapper, RadioButton, Label, Group, Select, Option, Errors} from './styles/form'
 
 const Form = ({children, ...restProps}) => <Wrapper {...restProps}>{children}</Wrapper>
 
-Form.Input = ({...restProps}) => <Input {...restProps} />
+Form.Input = ({...restProps}) => <Input type="text" {...restProps} />
 
 Form.TextArea = ({children, ...restProps}) => <TextArea {...restProps}>{children}</TextArea>
 
@@ -18,5 +18,7 @@ Form.Label = ({children, ...restProps}) => <Label {...restProps}>{children}</Lab
 Form.Select = ({children, ...restProps}) => <Select {...restProps}>{children}</Select>
 
 Form.Option = ({children, ...restProps}) => <Option {...restProps}>{children}</Option>
+
+Form.Errors = ({children, ...restProps}) => <Errors {...restProps}>{children}</Errors>
 
 export default Form
