e52ba98a33ef708a77605b31835062c525751874
Nico Plyley <nplyley@gmail.com>
2021-01-23 20:37:15 -0500

    Moved button to form

--- src/pages/Guardian.js
+++ src/pages/Guardian.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import {Container, Title, Text, Button} from '../components'
+import {Container, Title, Text} from '../components'
 import {GuardianForm} from '../sections/forms'
-import {Link} from 'react-router-dom'
 
 const Guardian = () => {
     return (
@@ -9,7 +8,6 @@ const Guardian = () => {
             <Title margin="0 0 5px 0">Guardian Info</Title>
             <Text margin="0 0 25px 0" color="red">* Indicates required fields</Text>
             <GuardianForm />
-            <Button margin="25px 0"><Link to="/emergency-contact">Continue</Link></Button>
         </Container>
     )
 }
