60738ed327ffca1ddb5d13d953d4248b50cc94ee
Nico Plyley <nplyley@gmail.com>
2021-01-24 17:52:48 -0500

    Updated date

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -17,7 +17,7 @@ const Success = () => {
     return (
         <Container>
             <Title>Success</Title>
-            <Text margin="25px 0 10px 0">Thank you for your payment we will see you on [insert date]</Text>
+            <Text margin="25px 0 10px 0">Thank you for your payment we will see you on 02/13/2021 at 6:00 PM</Text>
             <Text>If you have any question please email or call us.</Text>
             <Text margin="10px 0">(407) 619-6269 | brian@amazinganimalsinc.org</Text>
         </Container>
