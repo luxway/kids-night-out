d1fa50ad4b20cc314e49de928be3ec50b6a8f678
Nico Plyley <nplyley@gmail.com>
2021-01-25 15:49:57 -0500

    Added token and fixed relationships

--- api/models.py
+++ api/models.py
@@ -1,29 +1,39 @@
 from run import db
 
 
-class Form(db.Model):
+class Kids(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-
-    # Kids
     first_name = db.Column(db.String(50))
     last_name = db.Column(db.String(50))
     age = db.Column(db.Integer)
     allergies = db.Column(db.String(255))
     food = db.Column(db.String(5))
+    token = db.Column(db.String(255))
+    emergency_id = db.Column(db.Integer, db.ForeignKey('emergency.id'))
+    guardian_id = db.Column(db.Integer, db.ForeignKey('guardian.id'))
 
-    # Emergency Contact
-    emergency_first_name = db.Column(db.String(100))
-    emergency_last_name = db.Column(db.String(100))
-    emergency_phone = db.Column(db.String(100))
-    emergency_alt_phone = db.Column(db.String(100))
 
-    # Guardian
-    guardian_first_name = db.Column(db.String(100))
-    guardian_last_name = db.Column(db.String(100))
-    guardian_address = db.Column(db.String(255))
-    guardian_city = db.Column(db.String(100))
-    guardian_state = db.Column(db.String(100))
-    guardian_zip = db.Column(db.Integer(10))
-    guardian_email = db.Column(db.String(100))
-    guardian_phone = db.Column(db.String(100))
-    guardian_alt_phone = db.Column(db.String(100))
+class Emergency(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+    alt_phone = db.Column(db.String(100))
+    token = db.Column(db.String(255))
+    kids = db.relationship('Kids', backref='emergency')
+
+
+class Guardian(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    address = db.Column(db.String(255))
+    city = db.Column(db.String(100))
+    state = db.Column(db.String(100))
+    zip = db.Column(db.Integer)
+    email = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+    alt_phone = db.Column(db.String(100))
+    token = db.Column(db.String(255))
+    paid = db.Column(db.Boolean)
+    kids = db.relationship('Kids', backref='guardian')
