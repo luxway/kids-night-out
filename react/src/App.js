b2cc88aaca12c6bfc6dddb6353709ce5b9d4cff1
Nico Plyley <nplyley@gmail.com>
2021-01-25 15:51:43 -0500

    Added context for token and save status

--- react/src/App.js
+++ react/src/App.js
@@ -1,20 +1,25 @@
 import React, {useState, useEffect} from 'react'
+import uuid from 'react-uuid'
 import {Nav, Status} from './sections/main'
 import Theme from './Theme'
 import {GlobalStyle} from './GlobalStyles'
-import {AddChild, ChildList, EmergencyContact, Guardian, Payment, Success} from './pages'
+import {AddChild, ChildList, EmergencyContact, Guardian, Payment, Success, Error} from './pages'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 export const GuardianContext = React.createContext(null)
 export const EmergencyContext = React.createContext(null)
 export const ChildContext = React.createContext(null)
 export const ValidContext = React.createContext(null)
+export const TokenContext = React.createContext(null)
+export const SavedContext = React.createContext(null)
 
 const App = () => {
     const [guardian, setGuardian] = useState({state: 'Florida'})
     const [emergency, setEmergency] = useState({})
     const [child, setChild] = useState([])
     const [valid, setValid] = useState([])
+    const [token, setToken] = useState(null)
+    const [saved, setSaved] = useState(false)
 
     const handleUnload = e => {
         e.preventDefault()
@@ -23,9 +28,10 @@ const App = () => {
 
     useEffect(() => {
         window.addEventListener('beforeunload', handleUnload)
+        setToken(uuid())
 
         return () => window.removeEventListener('beforeunload', handleUnload)
-    })
+    }, [setToken])
 
     return (
 
@@ -37,17 +43,22 @@ const App = () => {
                     <EmergencyContext.Provider value={[emergency, setEmergency]}>
                         <ChildContext.Provider value={[child, setChild]}>
                             <ValidContext.Provider value={[valid, setValid]}>
-                            <Status />
-                                <Router>
-                                    <Switch>
-                                        <Route path="/" component={Guardian} exact />
-                                        <Route path="/emergency-contact" component={EmergencyContact} exact />
-                                        <Route path="/child" component={ChildList} exact />
-                                        <Route path="/add-child" component={AddChild} exact />
-                                        <Route path="/payment" component={Payment} exact />
-                                        <Route path="/success" component={Success} exact />
-                                    </Switch>
-                                </Router>
+                                <TokenContext.Provider value={[token]}>
+                                    <SavedContext.Provider value={[saved, setSaved]}>
+                                        <Status />
+                                        <Router>
+                                            <Switch>
+                                                <Route path="/" component={Guardian} exact />
+                                                <Route path="/emergency-contact" component={EmergencyContact} exact />
+                                                <Route path="/child" component={ChildList} exact />
+                                                <Route path="/add-child" component={AddChild} exact />
+                                                <Route path="/payment" component={Payment} exact />
+                                                <Route path="/success" component={Success} exact />
+                                                <Route path="/error" component={Error} exact />
+                                            </Switch>
+                                        </Router>
+                                    </SavedContext.Provider>
+                                </TokenContext.Provider>
                             </ValidContext.Provider>
                         </ChildContext.Provider>
                     </EmergencyContext.Provider>
