484b5db2964013b2d190b81a7b0e1c0d8fdba507
Nico Plyley <nplyley@gmail.com>
2021-01-25 15:50:20 -0500

    Added form handling

--- api/run.py
+++ api/run.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+CORS(app)
+
+import form
 
 
-@app.route('/api')
+@app.route('/api', methods=['POST'])
 def index():
-    return 'Test'
+    data = request.json
+    form.handle_data(data['data'])
+    return {'message': 'success'}, 200
