df54faa375f6a3fe085a1cc07b074ae9f7c145ec
Nico Plyley <nplyley@gmail.com>
2021-02-05 23:15:38 -0500

    Updated phone number

--- react/src/pages/Success.js
+++ react/src/pages/Success.js
@@ -19,7 +19,7 @@ const Success = () => {
             <Title>Success</Title>
             <Text margin="25px 0 10px 0">Thank you for your payment we will see you on 02/13/2021 at 6:00 PM</Text>
             <Text>If you have any question please email or call us.</Text>
-            <Text margin="10px 0">(407) 619-6269 | brian@amazinganimalsinc.org</Text>
+            <Text margin="10px 0">(407) 719-6269 | brian@amazinganimalsinc.org</Text>
         </Container>
     )
 }
